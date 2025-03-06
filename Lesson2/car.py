6732ddc15a103dfdcf1242d845e3624ac7f2dfc4
smithbwlhs <smithb@wlwv.k12.or.us>
2025-03-06 09:32:00 -0800

    updated

--- Lesson2/car.py
+++ Lesson2/car.py
@@ -1,8 +1,16 @@
+"""
+Name: Mr. Smith
+Date: 3/4/25
+Description: Very basic car class
+"""
+
+
 class Car:
-    '''
+    """
     A simple attempt to represent a car
-    '''
-    def __init__(self,make:str,model:str,year:int):
+    """
+
+    def __init__(self, make: str, model: str, year: int):
         """Constructor
 
         Args:
@@ -14,9 +22,8 @@ class Car:
         self.model = model
         self.year = year
 
-    def get_descriptive_name(self)->str:
+    def get_descriptive_name(self) -> str:
         """Returns a nicely formatted description of the car
-
 
         Returns:
             str: description of the car formatted in title case
@@ -24,9 +31,15 @@ class Car:
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
 
+
 def main():
-    my_new_car = Car('audi','a4',2025)
+    my_new_car = Car("audi", "a4", 2025)
+    print(my_new_car.get_descriptive_name())
+    my_new_car.year = 2026
+    my_new_car.make = "Rivian"
+    my_new_car.model = "R3X"
     print(my_new_car.get_descriptive_name())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
---
+++ Lesson2/car_v2.py
@@ -0,0 +1,64 @@
+"""
+Name: Mr. Smith
+Date: 3/4/25
+Description: A car with an odometer
+"""
+
+class Car:
+    '''
+    A simple attempt to represent a car
+    '''
+    def __init__(self,make:str,model:str,year:int):
+        """Constructor. Odometer is set to 0 for all new cars
+
+        Args:
+            make (str): Make of car
+            model (str): Model of car
+            year (int): Year the car was made
+        """
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer = 0
+
+    def get_descriptive_name(self)->str:
+        """Returns a nicely formatted description of the car
+
+        Returns:
+            str: description of the car formatted in title case
+        """
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self)->None:
+        """Prints the number of miles on the car
+        """
+
+        print(f"This car has {self.odometer} miles on it")
+    def update_odometer(self,mileage:int)->None:
+        if mileage >= self.odometer:
+            old_mileage = self.odometer
+            self.odometer = mileage
+            print(f"The odometer was updated from {old_mileage} to {mileage}.")
+        else:
+            print("You cannot roll back an odometer")
+    def increment_odometer(self,miles:int)->None:
+        if miles>=0:
+            self.odometer += miles
+        else:
+            print(f"You cannot rollback an odometer")
+        
+
+def main():
+    my_new_car = Car('audi','a4',2025)
+    print(my_new_car.get_descriptive_name())
+    my_new_car.update_odometer(100)
+    my_new_car.update_odometer(50)
+    my_new_car.increment_odometer(25)
+    my_new_car.read_odometer()
+    my_new_car.increment_odometer(-20)
+    my_new_car.read_odometer()
+    
+
+if __name__ == '__main__':
+    main()
