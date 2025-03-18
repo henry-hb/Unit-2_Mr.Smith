7f63226a9086647059f5413e2e00b54f8e128031
smithbwlhs <smithb@wlwv.k12.or.us>
2025-03-18 11:46:58 -0700

    added lesson 3

---
+++ Lesson4/television.py
@@ -0,0 +1,26 @@
+class Television:
+    def __init__(self,power_button):
+        self.power_button = False
+        self.channel = 3
+        self.volume = 5
+
+    def channel_up(self):
+        self.channel += 1
+    
+    def channel_down(self):
+        self.channel -= 1
+    
+    def return_channel(self):
+        return self.channel
+    
+    def volume_up(self):
+        self.volume += 1
+    
+    def volume_down(self):
+        self.volume -= 1
+
+    def get_volume(self):
+        return self.volume
+    
+    def __repr__(self):
+        return f"Channel: {self.channel}\nVolume: {self.volume}"
